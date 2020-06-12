8646bc7a743d233a3b9a83e4561a6993a2b47d00
TheLX <1361787281@qq.com>
2020-06-12 20:13:14 +0800

    testGit In Idea just for test002、 第二次测试

--- FirstGitTestProject/src/com/lx/First.java
+++ FirstGitTestProject/src/com/lx/First.java
@@ -3,9 +3,14 @@ package com.lx;
 public class First {
     public static void main(String[] args) {
         System.out.println("git test 0001");
+        System.out.println("实现修改");
     }
     public void test1(){
         int i=1;
+        System.out.println(i);
+    }
+    public void test2(){
+        int i=2;
         System.out.println(i);
     }
 }
