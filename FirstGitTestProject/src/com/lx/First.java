766297c96e3659611d6a1aa61b92fd94a548644d
TheLX <1361787281@qq.com>
2020-06-12 17:00:23 +0800

    testGit In Idea just for test

---
+++ FirstGitTestProject/src/com/lx/First.java
@@ -0,0 +1,7 @@
+package com.lx;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("git test 0001");
+    }
+}
---
+++ FirstGitTestProject/src/com/lx/model/User.java
@@ -0,0 +1,23 @@
+package com.lx.model;
+
+
+public class User {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    private String name;
+    private String sex;
+}
