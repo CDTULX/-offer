0d5a272b77cd28d1be7b5729de75ec5b40815751
TheLX <1361787281@qq.com>
2020-06-12 17:01:58 +0800

    testGit In Idea just for test002

--- FirstGitTestProject/src/com/lx/First.java
+++ FirstGitTestProject/src/com/lx/First.java
@@ -4,4 +4,8 @@ public class First {
     public static void main(String[] args) {
         System.out.println("git test 0001");
     }
+    public void test1(){
+        int i=1;
+        System.out.println(i);
+    }
 }
